b5b4544ae00cf810b84a42b0f0f409223292abfb
Smarak Das <dsmarak17@gmail.com>
2021-05-11 19:26:26 +0530

    feat(unit_test): Unit testing for Alarms State.
    Mockito is used for mocking dependencies of the providers.
    
    While testing a provider do not forget to override the dependencies of
    the provider with the mock, so that you can stub the methods from the
    dependencies.
    
    Override the dependencies by passing in a list of the overriden
    providers as an argument to `overrides` parameter of
    ProviderContainer(). See
    [this](https://riverpod.dev/docs/cookbooks/testing#overriding-the-behavior-of-a-provider-during-tests).
    
    Make sure that you do not reuse ProviderContainer between tests. Use the
    debugState property of on container.read(<provider>.notifier) to access
    the state inside the tests to use in expect.
    
    Remember to use GenerateMocks annotation from mockito or manually create
    the correct mocks compliant with null safety. Read more
    [here](https://github.com/dart-lang/mockito/blob/master/NULL_SAFETY_README.md).

--- pings/test/alarms_state_test.dart
+++ pings/test/alarms_state_test.dart
@@ -1,5 +1,85 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:io';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:pings/models/alarm.dart';
+import 'package:pings/providers/providers.dart';
+import 'package:pings/services/alarms_api_service.dart';
+import 'package:pings/state/alarm_state.dart';
+
+import 'alarms_state_test.mocks.dart';
+
+@GenerateMocks([AlarmsApi])
 void main() {
-  group('AlarmsStateTest - ', () {});
+  test('Verify return type of alarmsNotifierProvider is AlarmsState', () async {
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+    expect(container.read(alarmsNotifierProvider), isA<AlarmsState>());
+  });
+
+  group('getAlarmsApi test', () {
+    test('First state is loading state', () async {
+      final container = ProviderContainer();
+      addTearDown(container.dispose);
+      expect(
+          container.read(alarmsNotifierProvider), const AlarmsState.loading());
+    });
+
+    test(
+        'should return AlarmsState()(initial state) when empty list is received from API',
+        () async {
+      final api = MockAlarmsApi();
+
+      when(api.fetchAlarms()).thenAnswer((realInvocation) async => <Alarm>[]);
+      final container = ProviderContainer(
+        overrides: [
+          alarmsApiProvider.overrideWithProvider(Provider((ref) => api))
+        ],
+      );
+      await container.read(alarmsNotifierProvider.notifier).getAlarmsFromApi();
+      expect(container.read(alarmsNotifierProvider.notifier).debugState,
+          const AlarmsState());
+    });
+    test(
+        'should return AlarmsState.success(<list of alarms>)(initial state) when non-empty list is received from API',
+        () async {
+      final api = MockAlarmsApi();
+
+      final alarm = Alarm(
+          createdBy: 'test',
+          description: 'test',
+          datetime: DateTime.now(),
+          frequency: 'test',
+          numberOfPeople: 1);
+
+      when(api.fetchAlarms())
+          .thenAnswer((realInvocation) async => <Alarm>[alarm]);
+      final container = ProviderContainer(
+        overrides: [
+          alarmsApiProvider.overrideWithProvider(Provider((ref) => api))
+        ],
+      );
+      await container.read(alarmsNotifierProvider.notifier).getAlarmsFromApi();
+      expect(container.read(alarmsNotifierProvider.notifier).debugState,
+          AlarmsState.success([alarm]));
+    });
+    test(
+        'should return AlarmsState.error when exception is thrown from the API',
+        () async {
+      final api = MockAlarmsApi();
+      when(api.fetchAlarms()).thenThrow(const SocketException('test'));
+
+      final container = ProviderContainer(
+        overrides: [
+          alarmsApiProvider.overrideWithProvider(Provider((ref) => api))
+        ],
+      );
+
+      await container.read(alarmsNotifierProvider.notifier).getAlarmsFromApi();
+      expect(container.read(alarmsNotifierProvider.notifier).debugState,
+          const AlarmsState.error('test'));
+    });
+  });
 }
